0c74324926ad4d9f7ed89e79c4c71c89bfe53395
Sebastian Alsina <alsinas@me.com>
2020-12-31 21:38:13 -0500

    Teletool

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function IndexPage() {
         <>
             <div className='p-6 max-w-lg w-full mx-auto text-gray-700 space-y-4'>
                 <h1 className='text-4xl font-extrabold text-discord-blue'>Teletool</h1>
-                <p>Upload a file and enter a message to distribute to your Telegram channels.</p>
+                <p>Upload a file and enter a message to distribute to your Telegram groups.</p>
                 <form onSubmit={submit} className='space-y-4'>
                     <input
                         type='file'
