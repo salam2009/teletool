0dac612d8f77e8dd437dab1c59b377721150e2d9
Sebastian Alsina <alsinas@me.com>
2020-12-31 19:40:19 -0500

    Added Textarea

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 export default function IndexPage() {
     return (
         <>
-            <div className='p-6 max-w-sm w-full mx-auto text-gray-700 space-y-4'>
+            <div className='p-6 max-w-lg w-full mx-auto text-gray-700 space-y-4'>
                 <h1 className='text-4xl font-extrabold text-discord-blue'>Teletool</h1>
                 <p>Upload a file and enter a message to distribute to your Telegram channels.</p>
                 <form action='' className='space-y-4'>
                     <input type='file' className='border p-2 block w-full rounded' />
-                    <input type='text' className='border p-2 block w-full rounded' placeholder='Message' />
+                    <textarea type='text' className='border h-32 p-2 block w-full rounded' placeholder='Message' />
                     <input type='text' className='border p-2 block w-full rounded' placeholder='Password' />
                     <input
                         type='submit'
